0a45be32a1a659eb2f5319e4e0fe48aa3f1431a5
Ragini340 <ragini31bxr@gmail.com>
2022-11-29 12:28:19 +0530

    Add project files.

---
+++ NullConditionalOperator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullConditionalOperatorPOC
+{
+    class NullConditionalOperator
+    {
+     static void Main(String[] args)
+       {
+            Employee obj = null;
+            obj = new Employee() {FirstName = "Ragini",LastName = "Singh"};
+            GetEmpDetails(obj);
+     }
+        public static void GetEmpDetails(Employee emp)
+        {
+            Console.WriteLine("Employee Details are:");
+            Console.WriteLine(emp?.FirstName); 
+            Console.WriteLine(emp?.LastName); 
+        }
+    }
+
+}
+    
